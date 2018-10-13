00834918c9616bd3ae15cb5ba8581e7618a48e1b
Manuel Brizuela <manuelhcelis@hotmail.com>
2018-10-12 23:36:30 -0430

    prueba Please enter the commit message for your changes. Lines starting

---
+++ js/js.js
@@ -0,0 +1,13 @@
+﻿// Este Script hace que cuando el scroll es mayor al tamaño de la imagen el se queda pegado arriba 
+$(document).ready(function(){
+	var altura = $('.menu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.menu').addClass('menu-fixed');
+		} else {
+			$('.menu').removeClass('menu-fixed');
+		}
+	});
+ 
+});
